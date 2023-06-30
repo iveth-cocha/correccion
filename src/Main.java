6cea634c458d263897e63c9987bf3659d2993151
iveth <maria.iveth16outlook.es>
2023-06-30 12:26:30 -0500

    llenado e impresion de de array

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import javax.sound.midi.Soundbank;
 import  java .util.ArrayList;
+
+
 
 public class Main {
     public static void main(String[] args) {
         //creaccion de array
-        ArrayList<Provincia>info=new ArrayList<Provincia>();
+        ArrayList<Provincia> info = new ArrayList<Provincia>();
 
-        //info.add( new(Provincia (593, "Ecuador","Insular","Galapagos")));
+        //llenar array
+
+        info.add(new Provincia(593, "Ecuador", "Insular", "Galapagos"));
+        info.add(new Provincia(51, "Mexico", "Costa", "chiapas"));
+
+        //Imprimir array
+
+       for (int i=0; i<info.size();i++){
+           System.out.println("Datos Provincia"+ info.get(i));
+       }
 
 
     }
